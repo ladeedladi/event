d9cdd96c1f354bad9c722b8b5008a6617eae321c
muhammed ladeed <81908636+ladeedladi@users.noreply.github.com>
2021-04-10 10:41:35 +0530

    local storage created

---
+++ localstorage.js
@@ -0,0 +1,13 @@
+localStorage.setItem('name','ladi')
+undefined
+localStorage.setItem('name','la')
+undefined
+localStorage.removeItem('name')
+undefined
+localStorage.clear[]
+VM358:1 Uncaught SyntaxError: Unexpected token ']'
+localStorage.clear()
+undefined
+console.log('a')
+VM440:1 a
+undefined
